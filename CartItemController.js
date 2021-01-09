b9a4c1bcdd1af2ae7570cf7f7e522335848dffc8
Anjan1016 <42114318+Anjan1016@users.noreply.github.com>
2021-01-09 22:47:15 +0530

    Create CartItemController.js

---
+++ CartItemController.js
@@ -0,0 +1,20 @@
+({
+	deleteCartItem : function(component, event, helper) {
+		var cartItemId = event.currentTarget.id;
+      
+        var action = component.get('c.deleteItem');
+        action.setParams({
+            "CartItemId" : cartItemId
+        });
+        action.setCallback(this, function(response){
+            var state = response.getState();
+           
+            if(state === 'SUCCESS'){
+                $A.get('e.force:refreshView').fire(); 
+            }else{
+                
+            }
+        });
+        $A.enqueueAction(action);
+	}
+})
